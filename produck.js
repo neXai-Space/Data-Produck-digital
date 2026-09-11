bb08f25b37516d43a1aa07a858cfadf9d071441e
neXai-Space <pitnewbie12@gmail.com>
2026-09-11 13:15:23 +0700

    Update produck.js

--- produck.js
+++ produck.js
@@ -1,14 +1,14 @@
 const PRODUCTS = [
   {
-    id: "1", // <- ID-nya "1"
+    id: "2", // <- ID-nya "1"
     name: "Netflix Premium",
     price: 45000,
     category: "Akun Premium"
   },
   {
-    id: "2", // <- ID-nya "2"
-    name: "Alight Motion Prem",
-    price: 98000,
+    id: "1", // <- ID-nya "2"
+    name: "Am Prem",
+    price: 1000,
     category: "Voucher"
   },
   {
