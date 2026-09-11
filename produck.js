40d78e19c8820aa9b8e64d38d3f10eba3fc7c997
neXai-Space <pitnewbie12@gmail.com>
2026-09-11 16:15:53 +0700

    Update produck.js

--- produck.js
+++ produck.js
@@ -19,8 +19,7 @@ const PRODUCTS = [
     originalPrice: 125000,
     category: "Akun Premium",
     icon: "🗂️",
-    description: "Akun Capcut Premium privat, garansi 30 hari, eksport video sampai HD 2K.
-    harga : 25rb/bulan, 10rb/minggu",
+    description: "Akun Capcut Premium privat, garansi 30 hari, eksport video sampai HD 2K, harga : 25rb/bulan, 10rb/minggu",
     rating: 4.5,
     sold: 50,
     sellerWA: "6282183882260"
