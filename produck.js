63af4c377df8877fd395942c5e841eac46bcbe1c
neXai-Space <pitnewbie12@gmail.com>
2026-09-11 13:40:36 +0700

    Update produck.js

--- produck.js
+++ produck.js
@@ -3,24 +3,48 @@ const PRODUCTS = [
     id: "2", // <- ID-nya "1"
     name: "Netflix Premium",
     price: 45000,
-    category: "Akun Premium"
+    category: "Akun Premium",
+    originalPrice: 65000,
+    category: "Akun Premium",
+    icon: "🎬",
+    description: "Akun Netflix Premium privat, garansi 30 hari, kualitas Ultra HD 4K, bisa dipakai di 4 perangkat sekaligus.",
+    rating: 4.9,
+    sold: 180,
   },
   {
     id: "3", // <- ID-nya "2"
     name: "Capcut Prem",
     price: 25000,
-    category: "Akun Premium"
+    category: "Akun Premium",
+    originalPrice: 125000,
+    category: "Akun Premium",
+    icon: "🗂️",
+    description: "Akun Capcut Premium privat, garansi 30 hari, eksport video sampai HD 2K.",
+    rating: 4.5,
+    sold: 180,
   },
   {
     id: "1", // <- ID-nya "2"
     name: "Am Prem by PitNewbie",
     price: 5000,
-    category: "Akun Premium"
+    category: "Akun Premium",
+    originalPrice: 120000,
+    category: "Akun Premium",
+    icon: "🎬",
+    description: "Akun alight motoon Premium privat, import preset hingga 200mb+, bisa dipakai di 4 perangkat sekaligus.",
+    rating: 5.2,
+    sold: 40,
   },
   {
     id: "1", // <- ID-nya "2"
     name: "Am Prem by RinnStore",
     price: 10000,
-    category: "Akun Premium"
+    category: "Akun Premium",
+    originalPrice: 120000,
+    category: "Akun Premium",
+    icon: "🎬",
+    description: "Akun alight motoon Prem, udh itu aja.",
+    rating: 5.0,
+    sold: 50,
   }
 ];
