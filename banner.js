b4a06cf06b2272e2f3fd6e69c026a87dc1662106
neXai-Space <pitnewbie12@gmail.com>
2026-09-12 15:59:54 +0700

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,13 @@
+const BANNERS = [
+  {
+    tagColor: "", // Kosongkan agar pakai warna bawaan (kuning/emas)
+    title: "Produk digital, ToLine",
+    desc: "Layanan wa, proses cepat, aman dan terpercaya."
+  },
+  {
+    tagColor: "#3b82f6", // Warna biru
+    title: "Ingin produk digital kamu diupload?",
+    // Kamu bisa langsung memasukkan tag <a> HTML di dalam teks deskripsi!
+    desc: "<a href='https://wa.me/6285727658632?text=Halo%20Min,%20saya%20ingin%20mengajukan%20produk%20digital%20saya' target='_blank' class='banner-link'>Klik disini</a> untuk mengajukan produk digital kamu."
+  }
+];
